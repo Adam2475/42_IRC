b72785125ce17f3913795eb2b2daa3a28f12ca61
Adam Natan Passaretti <adapassa@c2r4p13.42firenze.it>
2025-09-29 17:50:51 +0200

    started building socket object

---
+++ include/Socket.hpp
@@ -0,0 +1,13 @@
+#ifndef SOCKET_HPP
+#define SOCKET_HPP
+
+#include <sys/socket.h>
+
+class socke
+{
+    public:
+        Socket(int domain, int type, int protocol);
+        //SOCK_STREAM type = (virtual circuit service)
+};
+
+#endif
